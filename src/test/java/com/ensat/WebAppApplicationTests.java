25730ded18938a0b62bfe357bda5b3d3920227df
tanphan0709 <tanphan0709@gmail.com>
2024-12-16 19:29:39 +0700

    Update WebAppApplicationTests.java

--- src/test/java/com/ensat/WebAppApplicationTests.java
+++ src/test/java/com/ensat/WebAppApplicationTests.java
@@ -1,16 +1,15 @@
 package com.ensat;
 
-import org.junit.*;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.*;
-import org.mockito.junit.jupiter.*;
-import org.springframework.test.context.web.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @ExtendWith(MockitoExtension.class)
 @WebAppConfiguration
 public class WebAppApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 }
